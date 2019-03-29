1586ed0e3d645fb56f01f4420cfe22c75cd63bcf
bemerson <bemerson@gracehill.com>
2019-03-28 22:13:39 -0400

    added show action and view: individual posts now showing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts_controller/new'
+  get 'posts_controller/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
 
