8b1c8e3e2c8c0ed44787e77cafc00c2789d20200
bemerson <bemerson@gracehill.com>
2019-03-28 21:22:35 -0400

    added markup and styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path 
+    redirect_to posts_path
   end
 
   private
